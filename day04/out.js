c5e0c62d6d36ea566a69b9c906ba582ab680b131
tkdcksqkr98 <82599643+tkdcksqkr98@users.noreply.github.com>
2023-02-02 17:41:36 +0900

    day04 project
    java script exercise
    (if, else, else if), for, funtion,  document.getElementById.value, div_tag.innerHTML, <buuton onclick=" _____()"

---
+++ day04/out.js
@@ -0,0 +1,20 @@
+        function 빨래하다() {
+            console.log("1. 먼지를 털자.")
+            console.log("2. 물에 집어 넣는다.")
+            console.log("3. 비누를 바른다.")
+            console.log("4. 오물조물 비빈다.")
+            console.log("5. 물에 여러번 행군다.")
+            console.log("6. 짠다.")
+            console.log("7. 넌다.")
+        }
+        빨래하다() 
+
+        //청소하다에 대한 함수정의해서 사용해보세요.
+        function 청소하다() {
+            console.log("1. 옷을 정리한다.")
+            console.log("2. 책을 정리한다.")
+            console.log("3. 먼지을 치운다.")
+            console.log("4. 청소기를 사용한다.")
+            
+        }
+        청소하다()
---
+++ day04/out2.js
@@ -0,0 +1,19 @@
+        function 여행하다() {
+            console.log("1. 계획을 짠다.")
+            console.log("2. 숙소 예매한다.")
+            console.log("3. 교통편을 확인 후 예매한다.")
+            console.log("4. 짐을 챙긴다.")
+            console.log("5. 여행에 출발한다.")
+            
+        }
+        여행하다() 
+
+        
+        function 쉬다() {
+            console.log("1. 격렬히 눕는다.")
+            console.log("2. 끝내주게 숨쉰다.")
+            console.log("3. 열정적으로 잔다.")
+            
+            
+        }
+        쉬다()
